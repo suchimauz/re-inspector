4cdddca6541ce83a2399634b0d0fce22739f4f5e
suchimauz <alexey.fedulov98@gmail.com>
2019-08-15 02:02:46 +0300

    move app-db 2-form from alkona

--- src/re_inspector/core.cljs
+++ src/re_inspector/core.cljs
@@ -2,8 +2,19 @@
   (:require
    [re-frame.core :as rf]))
 
-(defn port-connect-extension [idx obj]
+(defn port-connect-extension [obj]
   ((.. js/chrome -runtime -connect) idx (clj->js obj)))
+
+(def Port
+  ((.. js/chrome -runtime -connect) idx (clj->js obj)))
+
+(defn app-db [idx]
+  (let [db (rf/subscribe [:re-inspector.core/app-db])
+        connect (re-ins/port-connect-extension "hlnclcmchflhfmogikmhblmjcffkggia" {:name "Alkona"})]
+    (fn []
+      (let [db @db
+            post (.postMessage connect {:data "Gfsdfsddf"})]
+        [:div "lok"]))))
 
 (rf/reg-sub
  ::app-db
